46760cf4a5d370e3a8df94d816b70286f87174b9
SaFaJim <46993549+SaFaJim@users.noreply.github.com>
2019-01-25 16:02:39 +0800

    Add files via upload

---
+++ AsynFlag.js
@@ -0,0 +1,26 @@
+﻿function AsynFlag() {
+    if (typeof this.setFlag != "function") {
+        AsynFlag.prototype.setFlag = function (obj, name, fun) {
+            if (obj.hasOwnProperty(name)) {
+                obj[name + "_fun"] = fun;
+                return;
+            }
+            obj[name] = 0;
+            obj[name + "_"] = 0;
+            Object.defineProperty(obj, name, {
+                get: function () {
+                    return obj[name + "_"];
+                },
+                set: function (value) {
+                    if (value != obj[name + "_"]) {
+                        obj[name + "_"] = value;
+                    }
+                    if (obj[name + "_fun"] == null) {
+                        obj[name + "_fun"] = fun;
+                    }
+                    obj[name + "_fun"]();
+                }
+            });
+        };
+    }
+}
